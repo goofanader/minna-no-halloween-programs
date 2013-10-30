e2acc40f55cfbe4885c517a366becb3633c4b568
goofanader@gmail.com <goofanader@gmail.com@03b3d6f7-c694-5c2d-aa24-47f246ff5f5e>
2013-10-30 15:28:22 +0000

    committing to current state

--- trunk/AnimeHalloweenPrograms/src/animehalloweenprograms/animetrivia/boardcreation/Category.java
+++ trunk/AnimeHalloweenPrograms/src/animehalloweenprograms/animetrivia/boardcreation/Category.java
@@ -18,14 +18,23 @@ public class Category {
    private int difficulty;
    private String title;
    private String description;
+   private String author;
 
    public Category() {
       questions = new String[Main.TOTAL_QUESTIONS];
       answers = new String[Main.TOTAL_QUESTIONS];
       series = new String[Main.TOTAL_QUESTIONS];
-      title = description = "";
+      title = description = author = "";
       difficulty = 0;
    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
     public String[] getSeries() {
         return series;
